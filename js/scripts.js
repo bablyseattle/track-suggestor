358586c93f167fb9d1bafb4cd2bc495ebea16b9c
Bably Kumari <bablyhyd@gmail.com>
2017-08-11 13:07:23 -0700

    add js to submit

--- js/scripts.js
+++ js/scripts.js
@@ -8,8 +8,33 @@ $(document).ready(function(){
     var select5  = $("input:radio[name=ques5]:checked").val();
     $("#nameInput").text(nameInput);
     alert(nameInput);
-    if(select1 === "front"){
-      $('#intro').show();
+    if(select1 === "front" && select2 === "yes"){
+      $('.result').hide();
+      $('.must').show();
+      $('#css').show();
+      $('#java').show();
+      $('#dotnet').show();
+      }
+    else if(select1 === "front" && select2 === "no" ){
+      $('.result').hide();
+      $('.must').show();
+      $('#css').show();
+      $('#dotnet').show();
+      $('#ruby').show();
+      }
+    else if(select1 === "back" && select2 === "yes" ){
+      $('.result').hide();
+      $('.must').show();
+      $('#java').show();
+      $('#dotnet').show();
+      $('#php').show();
+      }
+    else{
+      $('.result').hide();
+      $('.must').show();
+      $('#dotnet').show();
+      $('#php').show();
+      $('#ruby').show();
     }
     (event).preventDefault();
   });

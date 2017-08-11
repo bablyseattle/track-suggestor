4b7426b2f95daaf5e4c277da2f3ddf1b3f487ac7
Bably Kumari <bablyhyd@gmail.com>
2017-08-11 11:56:49 -0700

    show answers

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,16 @@
 $(document).ready(function(){
   $("#quiz").submit(function(event){
-    var intro = $("input:radio[name=intro]:checked").val();
-    if(intro === "yes" || intro === "no2"){
-      $("#quiz").hide();
-      $("#decide").show();
+    var nameInput = $("input#name").val();
+    var select1  = $("input:radio[name=ques1]:checked").val();
+    var select2 = $("input:radio[name=ques2]:checked").val();
+    var select3 = $("input:radio[name=ques3]:checked").val();
+    var select4  = $("input:radio[name=ques4]:checked").val();
+    var select5  = $("input:radio[name=ques5]:checked").val();
+    $("#nameInput").text(nameInput);
+    alert(nameInput);
+    if(select1 === "front"){
+      $('#intro').show();
     }
-      else {
-        alert("All tracks must complete Intro and JavaScript.");
-      }
     (event).preventDefault();
   });
 });

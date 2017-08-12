36d5460373e0e0c54b053303d29ecb1410605076
Bably Kumari <bably@Bablys-MacBook-Pro.local>
2017-08-11 23:19:51 -0700

    show one suggestion instead of 3 in previous version

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,32 @@
 $(document).ready(function(){
   $("#quiz").submit(function(event){
     var nameInput = $("input#name").val();
-    var select1  = $("input:radio[name=ques1]:checked").val();
-    var select2 = $("input:radio[name=ques2]:checked").val();
-    var select3 = $("input:radio[name=ques3]:checked").val();
-    var select4  = $("input:radio[name=ques4]:checked").val();
-    var select5  = $("input:radio[name=ques5]:checked").val();
+    var ifDesign  = $("input:radio[name=ques1]:checked").val();
+    var ifAndroid = $("input:radio[name=ques2]:checked").val();
+    var ifWebApp = $("input:radio[name=ques3]:checked").val();
+    var ifWordpress  = $("input:radio[name=ques4]:checked").val();
+    var ifWindowsApp  = $("input:radio[name=ques5]:checked").val();
     $("#nameInput").text(nameInput);
     $("#quiz").slideToggle();
-    if(select1 === "yes" && select2 === "yes"){
+    if(ifDesign === "yes"){
       $('.result').hide();
       $('.must').show();
       $('#css').show();
-      $('#java').show();
-      $('#dotnet').show();
       }
-    else if(select1 === "yes" && select2 === "no"){
+    else if(ifAndroid === "yes"){
       $('.result').hide();
       $('.must').show();
-      $('#css').show();
-      $('#dotnet').show();
+      $('#java').show();
+      }
+    else if(ifWebApp === "yes"){
+      $('.result').hide();
+      $('.must').show();
       $('#ruby').show();
-      }
-    else if(select1 === "no" && select2 === "yes"){
-      $('.result').hide();
-      $('.must').show();
-      $('#java').show();
-      $('#dotnet').show();
-      $('#php').show();
       }
     else{
       $('.result').hide();
       $('.must').show();
       $('#dotnet').show();
-      $('#php').show();
-      $('#ruby').show();
     }
     (event).preventDefault();
   });
